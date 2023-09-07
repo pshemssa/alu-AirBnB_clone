ddbf3d2609507f27e001eb0c76d4069ccaeb2f1b
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-09-07 16:31:00 +0200

    console.py

--- console.py
+++ console.py
@@ -51,19 +51,7 @@ class HBNBCommand(cmd.Cmd):
         "Review"
     }
 
-    def do_quit(self, arg):
-        """
-            Quit command to exit the program.
-        """
-        return True
-
-    def do_EOF(self, arg):
-        """
-            EOF signal to exit the program.
-        """
-        print("")
-        return True
- def emptyline(self):
+    def emptyline(self):
         """
             Do nothing upon receiving an empty line
         """
@@ -91,7 +79,20 @@ class HBNBCommand(cmd.Cmd):
                     return argdict[command[0]](call)
         print("*** Unknown syntax: {}".format(arg))
         return False
-        
+
+    def do_quit(self, arg):
+        """
+            Quit command to exit the program.
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+            EOF signal to exit the program.
+        """
+        print("")
+        return True
+
     def do_create(self, arg):
         """
         Usage: create <class>
