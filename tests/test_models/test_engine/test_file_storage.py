b7b99550b52865ef77d93ce5f0cb1ce145cfe18f
pshemssa <122387932+pshemssa@users.noreply.github.com>
2023-09-07 14:22:12 +0200

    Filestorage

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -10,6 +10,12 @@ from models.engine.file_storage import FileStorage
 class TestCaseFileStorage(unittest.TestCase):
     """ class for test cases """
 
+   def test_pep8_Filestorage(self):
+       """tests pep8"""
+       style = pep8.StyleGuide(quite=True)
+       p = style.check_files(['models/engine/file_storage.py'])
+       self.assertEqual(p.total_errors, 0, "check pep8")
+       
     def setUp(self):
         """ setting up the various
             components for the test """
